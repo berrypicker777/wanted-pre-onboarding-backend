f31fcba7794600bd270daac91d5bc045f4530079
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-10-14 19:17:51 +0900

    Feat: RecruitmentService 클래스 생성

--- src/main/java/wanted/pre/onboardingbackend/service/RecruitmentService.java
+++ src/main/java/wanted/pre/onboardingbackend/service/RecruitmentService.java
@@ -1,2 +1,18 @@
-package wanted.pre.onboardingbackend.service;public class RecruitmentService {
+package wanted.pre.onboardingbackend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wanted.pre.onboardingbackend.model.*;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class RecruitmentService {
+    private final CompanyRepository companyRepository;
+    private final UserRepository userRepository;
+    private final RecruitmentRepository recruitmentRepository;
+    private final HistoryRepository historyRepository;
+
+
 }

cd9599d71303e24c8c2753a218345bd171ba83fb
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-10-14 18:59:39 +0900

    Feat: History model 및 repository 생성

---
+++ src/main/java/wanted/pre/onboardingbackend/model/History.java
@@ -0,0 +1,42 @@
+package wanted.pre.onboardingbackend.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "history_tb")
+@Entity
+public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Recruitment recruitment; // 채용 공고
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user; // 사용자
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/wanted/pre/onboardingbackend/model/HistoryRepository.java
@@ -0,0 +1,6 @@
+package wanted.pre.onboardingbackend.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HistoryRepository extends JpaRepository<History, Long> {
+}

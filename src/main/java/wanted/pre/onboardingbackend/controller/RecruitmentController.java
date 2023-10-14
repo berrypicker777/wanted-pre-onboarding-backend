6561570d32d0eb1a4f249437c87f2dbf0e76a52f
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-10-14 19:11:41 +0900

    Feat: RecruitmentContoller 클래스 생성

---
+++ src/main/java/wanted/pre/onboardingbackend/controller/RecruitmentController.java
@@ -0,0 +1,12 @@
+package wanted.pre.onboardingbackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class RecruitmentController {
+    private final RecruitmentService recruitmentService;
+
+
+}

59c74bf7f560dcec5ebc5a7f400f32915e032326
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-10-14 20:14:25 +0900

    Feat: RecruitmentContoller 클래스에 addRecruitment 메서드 생성

--- src/main/java/wanted/pre/onboardingbackend/controller/RecruitmentController.java
+++ src/main/java/wanted/pre/onboardingbackend/controller/RecruitmentController.java
@@ -1,12 +1,23 @@
 package wanted.pre.onboardingbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import wanted.pre.onboardingbackend.dto.ResponseDTO;
+import wanted.pre.onboardingbackend.dto.recruitment.RecruitmentRequest;
+import wanted.pre.onboardingbackend.service.RecruitmentService;
 
 @RequiredArgsConstructor
 @RestController
 public class RecruitmentController {
     private final RecruitmentService recruitmentService;
 
+    @PostMapping("/company/{companyId}/recruitment")
+    public ResponseDTO addRecruitment(@PathVariable Long companyId, @RequestBody RecruitmentRequest.AddDTO addDTO) {
+        recruitmentService.addRecruitment(companyId, addDTO);
 
+        return new ResponseDTO<>();
+    }
 }

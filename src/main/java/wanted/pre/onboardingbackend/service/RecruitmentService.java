9016d187f3b309205b6e1d7744ff493c6d5b3e0e
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-10-14 20:54:53 +0900

    Feat: RecruitmentService 클래스에 addRecruitment 메서드 추가

--- src/main/java/wanted/pre/onboardingbackend/service/RecruitmentService.java
+++ src/main/java/wanted/pre/onboardingbackend/service/RecruitmentService.java
@@ -3,6 +3,9 @@ package wanted.pre.onboardingbackend.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import wanted.pre.onboardingbackend.core.exception.Exception404;
+import wanted.pre.onboardingbackend.core.exception.Exception500;
+import wanted.pre.onboardingbackend.dto.recruitment.RecruitmentRequest;
 import wanted.pre.onboardingbackend.model.*;
 
 @Transactional(readOnly = true)
@@ -15,5 +18,16 @@ public class RecruitmentService {
     private final RecruitmentRepository recruitmentRepository;
     private final HistoryRepository historyRepository;
 
+    public void addRecruitment(Long companyId, RecruitmentRequest.AddDTO addDTO) {
+        Company companyPS = companyRepository.findById(companyId).orElseThrow(
+                () -> new Exception404("해당 회사가 존재하지 않습니다.")
+        );
+
+        try {
+            recruitmentRepository.save(addDTO.toEntity(companyPS));
+        } catch (Exception e) {
+            throw new Exception500(e.getMessage());
+        }
+    }
 
 }
